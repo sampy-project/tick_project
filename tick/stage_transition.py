4aa28d1ac723dd37ebb0f8cfef48952c97c19357
mrgoodfish <fviard440@gmail.com>
2023-08-29 17:33:09 -0400

    finished proportion based transition

--- tick/stage_transition.py
+++ tick/stage_transition.py
@@ -19,22 +19,63 @@ class ProportionBasedStageTransition:
         if disease_status not in ['infected', 'susceptible']:
             raise ValueError("disease_status should either be 'infected' or 'susceptible'.")
         for key, val in dict_transitions.items():
+            if not (0. <= val <= 1.):
+                raise ValueError("Values in the dictionnary should be floats between 0 and 1.")
             start_phase, start_stage, end_phase, end_stage = key
             if start_phase not in ['egg', 'larva', 'nymph', 'adult']:
                 raise ValueError("Keyes of the dict should be tuples of the form (start_phase," + 
                                  " start_stage, end_phase, end_stage), with phases being in" + 
                                  " [egg, larva, nymph, adult].")
+            if (start_phase == 'egg') and not (0 <= start_stage < self.stages_as_tupple[0]):
+                raise ValueError("The starting phase is invalid.")
+            if (start_phase == 'larva') and not (0 <= start_stage < self.stages_as_tupple[1]):
+                raise ValueError("The starting phase is invalid.")
+            if (start_phase == 'nymph') and not (0 <= start_stage < self.stages_as_tupple[2]):
+                raise ValueError("The starting phase is invalid.")
+            if (start_phase == 'adult') and not (0 <= start_stage < self.stages_as_tupple[3]):
+                raise ValueError("The starting phase is invalid.")
+
             if end_phase not in ['egg', 'larva', 'nymph', 'adult']:
                 raise ValueError("Keyes of the dict should be tuples of the form (start_phase," + 
                                  " start_stage, end_phase, end_stage), with phases being in" + 
                                  " [egg, larva, nymph, adult].")
+            if (end_phase == 'egg') and not (0 <= end_stage < self.stages_as_tupple[0]):
+                raise ValueError("The ending phase is invalid.")
+            if (end_phase == 'larva') and not (0 <= end_stage < self.stages_as_tupple[1]):
+                raise ValueError("The ending phase is invalid.")
+            if (end_phase == 'nymph') and not (0 <= end_stage < self.stages_as_tupple[2]):
+                raise ValueError("The ending phase is invalid.")
+            if (end_phase == 'adult') and not (0 <= end_stage < self.stages_as_tupple[3]):
+                raise ValueError("The ending phase is invalid.")
             
 
     def proportion_based_transitions(self, feeding_status, disease_status, dict_transitions):
+        """
+        Performs the transitions encoded in the dictionary 'dict_transitions'. Its keyes are tuples of the form
+        (start_phase, start_stage, end_phase, end_stage), where start_phase and end_phase are string in the list
+        ['egg', 'larva', 'nymph', 'adult'] and start_stage and end_stage are integers giving a valid stage
+        for each corresponding phase. The values are non-neg floats giving the proportion of the population that 
+        transitions.
+
+        IMPORTANT: There cannot be any "loop" in the transitions.
+
+        :param feeding_status: string, either 'fed' or 'unfed'.
+        :param disease_status: string, either 'infected' of 'susceptible'.
+        :param matrix_transitions: 2D array of floats. matrix_transitions[i, j] is the proportion of ticks that should
+                                   go from status i to status j. Note that the method will be more efficient if there
+                                   are only transitions such that j > i.
+        """
         matrix_transitions = np.full((self.pop_per_vertex_fed.shape[1],
                                       self.pop_per_vertex_fed.shape[1]), 0.)
         for key, val in dict_transitions.items():
             start_phase, start_stage, end_phase, end_stage = key
+
+            index_start_stage = getattr(self, 'indexes_' + start_phase + '_stage')[start_stage]
+            index_end_stage = getattr(self, 'indexes_' + end_phase + '_stage')[end_stage]
+
+            matrix_transitions[index_start_stage, index_end_stage] = val
+
+        self.proportion_based_transition_from_matrix(feeding_status, disease_status, matrix_transitions)
 
     def _sampy_debug_proportion_based_transition_from_matrix(self, feeding_status, disease_status, matrix_transitions):
         if feeding_status not in ['unfed', 'fed']:
@@ -45,6 +86,8 @@ class ProportionBasedStageTransition:
             raise ValueError("The transition matrix has wrong shape.")
         if matrix_transitions.shape[0] != matrix_transitions.shape[1]:
             raise ValueError("The transition matrix should be square.")
+        if (matrix_transitions < 0.).any() or (matrix_transitions > 1.).any():
+            raise ValueError("The transition matrix has values that are not between 0 and 1.")
         
     def proportion_based_transition_from_matrix(self, feeding_status, disease_status, matrix_transitions):
         """
