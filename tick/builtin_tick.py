cedbf668c47042946b75ebb095c5381c44ea7f0f
mrgoodfish <fviard440@gmail.com>
2023-08-30 21:59:36 -0400

    add feeding building block

--- tick/builtin_tick.py
+++ tick/builtin_tick.py
@@ -1,17 +1,21 @@
 from .base import BaseTickFourPhases
 from .stage_transition import ProportionBasedStageTransition
 from .mortality import ProportionBasedTickMortality
+from .feeding import FeedingSingleGraph
 from sampy.utils.decorators import sampy_class
 
 
 @sampy_class
 class BasicTick(BaseTickFourPhases,
                 ProportionBasedStageTransition,
-                ProportionBasedTickMortality):
+                ProportionBasedTickMortality,
+                FeedingSingleGraph):
     """
-    First iteration of a tick for SamPy. Provides basic methods for population dynamics.
-    Theoretically, this class provides enough tool to satisfy most modelization needs,
-    but using it as it is now may require quite a lot of customization by the user.
+    First iteration of a tick for SamPy. Provides basic methods for tick population dynamics.
+    This class assumes that the tick population and all the hosts share the same graph.
+
+    IMPORTANT: Theoretically, this class provides enough tool to satisfy most modelization needs,
+               but using it as it is now may require quite a lot of customization by the user.
     """
     def __init__(self, **kwargs):
         pass
---
+++ tick/feeding.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class FeedingSingleGraph:
+    """
+    This building-blocks provides tool to link the population of ticks to a single or several hosts, 
+    on which they will feed. This building block assumes that the various hosts and the ticks share 
+    the same graph.
+    """
+    def __init__(self, **kwargs):
+        pass
